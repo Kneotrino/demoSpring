8483e37a6bfcbb81daf5ac74c32be3b05ff353f9
Neutrino Kusrorong <kneotrino@yahoo.com>
2019-08-21 12:14:52 +0700

    jooq 3.9.1

--- src/main/java/com/example/demo/BlogController.java
+++ src/main/java/com/example/demo/BlogController.java
@@ -1,14 +1,63 @@
 package com.example.demo;
 
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.conf.Settings;
+import org.jooq.impl.DSL;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Date;
 
 @RestController
 public class BlogController {
 
     @RequestMapping("/")
     public String index() {
-        return "Congratulations from BlogController.java";
+
+        Connection conn = null;
+        String userName = "root";
+        String password = "admin";
+        String url = "jdbc:mysql://localhost:3306/classicmodels";
+        Result<Record> result = null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(url, userName, password);
+
+//            DSLContext dslContext = DSL.using(conn, SQLDialect.MYSQL);
+
+            DSLContext pretty = DSL.using(conn, new Settings().withRenderFormatted(true));
+
+            result = pretty.select().from("customers").limit(10).fetch();
+
+            for (Record r : result) {
+                stringBuilder.append(r.formatJSON());
+            }
+            conn.close();
+        } catch (Exception e) {
+            // You'll probably want to handle the exceptions in a real app
+            // Don't ever do this silence catch(Exception e) thing. I've seen this in
+            // live code and it is horrendous.
+            e.printStackTrace();
+        }
+
+
+
+
+        return result.formatJSON();
+
+//        return "Hello World.java " + new Date().toString();
     }
 
+    @RequestMapping("/Custumer")
+    public String Custumer() {
+
+        return "Hello World.java";
+    }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,86 @@
 package com.example.demo;
 
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.*;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.List;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
+	//Test Mongo Driver
+//	@Test
 	public void contextLoads() {
+		System.out.printf("hello");
+		// Creating a Mongo client
+//		MongoClient mongo = new MongoClient( "localhost" , 27017 );
+		MongoClient mongoClient = MongoClients.create();
+
+		// Creating Credentials
+		MongoCredential credential;
+		credential = MongoCredential.createCredential("sampleUser", "myDb",
+				"password".toCharArray());
+		System.out.println("Connected to the database successfully");
+
+		// Accessing the database
+		MongoDatabase database = mongoClient.getDatabase("myDb");
+		System.out.println("Credentials ::"+ credential);
 	}
 
+	@Test
+	public void generateMysqlJooq()
+	{
+		System.out.println("DemoApplicationTests.generateMysqlJooq");
+		Configuration configuration = new Configuration()
+				.withJdbc(new Jdbc()
+						.withDriver("com.mysql.jdbc.Driver")
+						.withUrl("jdbc:mysql://localhost:3306/classicmodels")
+						.withUser("root")
+						.withPassword("admin"))
+				.withGenerator(new Generator()
+						.withName("org.jooq.util.DefaultGenerator")
+						.withDatabase(new Database()
+								.withName("classicmodels")
+								.withIncludes(".*")
+								.withSchemata(new Schema().withInputSchema("your_schema"))
+						)
+						.withTarget(new Target()
+								.withPackageName("jooq.generate")
+								.withDirectory("src/main/java")));
+		try {
+			GenerationTool.generate(configuration);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+//	@Test
+	public void redisDriverTest() {
+		System.out.println("Redis Test");
+		Jedis jedis = new Jedis("localhost");
+		System.out.println("Connection to server sucessfully");
+		//check whether server is running or not
+		System.out.println("Server is running: "+jedis.ping());
+
+		String s = jedis.get("user:123");
+		System.out.println("s = " + s);
+
+		//store data in redis list
+		jedis.lpush("tutorial-list", "Redis");
+		jedis.lpush("tutorial-list", "Mongodb");
+		jedis.lpush("tutorial-list", "Mysql");
+		// Get the stored data and print it
+		List<String> list = jedis.lrange("tutorial-list", 0 ,5);
+
+		for(int i = 0; i<list.size(); i++) {
+			System.out.println("Stored string in redis:: "+list.get(i));
+		}
+	}
 }
